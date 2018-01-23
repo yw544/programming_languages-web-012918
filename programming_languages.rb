bf8204df3ba51cc1dcdf5c787a3875453ede5540
York Wang <yw544@nyu.edu>
2018-01-23 04:27:52 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,7 @@ def reformat_languages(languages)
   languages.each do |type, language|
     language.each do |name, data|
       new_hash[name] ||=data
-      new_hash[name][:style]|| = []
+      new_hash[name][:style]||=[]
       new_hash[name][:style]<<type
 end
 end

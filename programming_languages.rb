202b1548644e8afc162397a2803446ac1d77ffa7
York Wang <yw544@nyu.edu>
2018-01-23 04:28:02 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,12 @@
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each do |type, language|
+    language.each do |name, data|
+      new_hash[name] ||=data
+      new_hash[name][:style]||=[]
+      new_hash[name][:style]<<type
+end
+end
+new_hash
 end
